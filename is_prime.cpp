925626670066d74a8c89fa4e7f302bb1d61b38e3
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-30 22:08:55 +0900

    Add Miller-Raabin primality test

---
+++ is_prime.cpp
@@ -0,0 +1,22 @@
+// 素数判定
+//
+// 計算量：O(√N)
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_prime(long long n) {
+    if (n <= 1) return false;
+    for (long long p = 2; p * p <= n; p++) {
+        if (n % p == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    long long n;
+    cin >> n;
+    if (is_prime(n)) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
